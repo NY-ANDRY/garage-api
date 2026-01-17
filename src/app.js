bd43d378f854df6854658a6232dc04c7f85498e4
NY-ANDRY <nyandrypaulferdinah@gmail.com>
2026-01-17 17:54:04 +0300

    ok

--- src/app.js
+++ src/app.js
@@ -1,35 +1,38 @@
 import express from "express";
-import { initializeApp, credential as _credential, messaging } from "firebase-admin";
+import serverless from "serverless-http";
+import { initializeApp, credential, messaging } from "firebase-admin";
 
-let serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT;
+const app = express();
+
+/* 🔑 IMPORTANT : convertir la variable d’environnement en objet */
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 initializeApp({
-  credential: _credential.cert(serviceAccount),
+  credential: credential.cert(serviceAccount),
   databaseURL:
     "https://garage-44cc0-default-rtdb.europe-west1.firebasedatabase.app",
 });
 
-const token =
-  "dftpit-U03CkaeJITIfdT1:APA91bHiKcRnIBRNFivZhuBP0-pzGF3ak9DDVq23Dyd4-ek4Zwp3nIhGWIVmUiRJPbRNZIhpragvRC39tP3Sa3qUabzWi51WemzdLLF2KPRUdsD46lhUPxk";
+app.get("/send", async (req, res) => {
+  try {
+    const message = {
+      notification: {
+        title: "hehehehehe",
+        body: "hohohohoho",
+      },
+      webpush: {
+        fcmOptions: {
+          link: "http://www.google.com",
+        },
+      },
+      token: req.query.token, // 🔥 dynamique
+    };
 
-const message = {
-  notification: {
-    title: " hehehehehe ",
-    body: " hohohohoho ",
-  },
-  webpush: {
-    fcmOptions: {
-      link: "http://www.google.com",
-    },
-  },
-  token: token,
-};
+    const response = await messaging().send(message);
+    res.json({ success: true, response });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
 
-messaging()
-  .send(message)
-  .then((response) => {
-    console.log("Successfully sent message:", response);
-  })
-  .catch((error) => {
-    console.log("Error sending message:", error);
-  });
+export default serverless(app);
